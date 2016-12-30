0f073327bd22562bd2b3257d6d2ecd6ebaea8d39
molipate <antoine.febvre1@gmail.com>
2016-12-30 14:35:24 +0100

    Fucking java

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -13,14 +13,13 @@ import java.util.Objects;
  */
 public class DrawingPanel extends JPanel {
 
-    private Ressources r;
-
     private BufferedImage background;
+    private ImageIcon test;
+    private Ressources r;
 
     private int[] options;
     private String district;
     private String area;
-
     public DrawingPanel() {
         super();
 
@@ -37,6 +36,12 @@ public class DrawingPanel extends JPanel {
         options = op;
         district = dis;
         area = str;
+
+        //test = new ImageIcon(background);
+        //test = new ImageIcon(test.getImage().getScaledInstance(tmp_width, tmp_height, BufferedImage.SCALE_SMOOTH));
+
+        //JLabel picLabel = new JLabel(test);
+        //add(picLabel, BorderLayout.EAST);
     }
 
     protected void paintComponent(Graphics g){
@@ -44,13 +49,17 @@ public class DrawingPanel extends JPanel {
         super.paintComponent(g);
         ArrayList<Datas> d = r.getDatas();
 
-        //background = r.getMap(area);
-        //double scale = (700 / (double) background.getWidth());
+        System.out.println("Affichage");
 
-        //int tmp_width = (int) (background.getWidth() * scale);
-        //int tmp_height = (int) (background.getHeight() * scale);
+        background = r.getMap(area);
+        double scale = (700 / (double) background.getWidth());
 
-        //g.drawImage(background.getScaledInstance(tmp_width, tmp_height, BufferedImage.SCALE_SMOOTH), 300, (700 - tmp_height) / 2 - 20, null);
+        int tmp_width = (int) (background.getWidth() * scale);
+        int tmp_height = (int) (background.getHeight() * scale);
+
+        g.drawImage(background.getScaledInstance(tmp_width, tmp_height, 4), 300, (700 - tmp_height) / 2, null);
+
+        g.drawImage(r.getIcon(0), 300, 120, null);
 
         for (int i = 0; i < d.size(); i++) {
             Datas tmp = d.get(i);
@@ -59,7 +68,7 @@ public class DrawingPanel extends JPanel {
                 if(tmp.getCategorie() != -1) {
                     if (options[tmp.getCategorie()] == 1) {
                         System.out.println("Affichage : " + tmp.getCategorie());
-                        g.drawImage(r.getIcon(tmp.getCategorie()), tmp.getLatitude_z() + 42, tmp.getLongitude_z() - 303, null);
+                        g.drawImage(r.getIcon(tmp.getCategorie()), tmp.getLatitude_z() + 42, tmp.getLongitude_z(), null);
                     }
                 }
             }
